d447502b41071a526aa4a3e325d0fdc309b8be13
Evgeny P. Kurbatov <evgeny.p.kurbatov@gmail.com>
2018-05-02 19:05:26 +0300

    Minor fix.

--- STM32CubeMX.Amperka_Iskra_JS/Src/usbd_desc.c
+++ STM32CubeMX.Amperka_Iskra_JS/Src/usbd_desc.c
@@ -75,7 +75,7 @@
 #define USBD_LANGID_STRING     1033
 #define USBD_MANUFACTURER_STRING     "Evgeny P. Kurbatov"
 #define USBD_PID_FS     22336
-#define USBD_PRODUCT_STRING_FS     "Relascope"
+#define USBD_PRODUCT_STRING_FS     "LzScope"
 #define USBD_SERIALNUMBER_STRING_FS     "00000000001A"
 #define USBD_CONFIGURATION_STRING_FS     "CDC Config"
 #define USBD_INTERFACE_STRING_FS     "CDC Interface"
